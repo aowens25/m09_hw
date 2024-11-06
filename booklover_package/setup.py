a4233814e7c96fc7e541a52c37abc3623925e130
Alexander Owens <nep6zu@virginia.edu>
2024-11-06 15:47:52 -0500

    updating files

---
+++ booklover_package/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='booklover',      
+    version='1.0',
+    packages=find_packages(),
+    description='Homework',
+    author='Alexander Owens',
+    author_email='nep6zu@virginia.edu',
+    url='https://github.com/aowens25/m09_hw',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
